60d850d69bf85816a41d1403e6b03439764e8f07
sanrrone <sandrolvalenzuelad@gmail.com>
2019-07-02 13:06:14 -0400

    more analysis in user interface

--- ui.R
+++ ui.R
@@ -1,10 +1,13 @@
 library(shiny)
+library(shinyFiles)
 library(shinyWidgets)
 library(shinydashboard)
 library(shinydashboardPlus)
+library(base64enc)
 source("Modules/header_ui.R")
 
 dashboardPagePlus(
+  skin = "blue-light",
   header = headerUIm("headerUImodule"),
   sidebar = dashboardSidebar(
     sidebarMenu(
@@ -13,6 +16,13 @@ dashboardPagePlus(
     )
   ),
   body = dashboardBody(
+    tags$head(tags$style(HTML('
+      .main-header .logo {
+        font-family: "Georgia", Times, "Times New Roman", serif;
+        font-weight: bold;
+        font-size: 25px;
+      }'
+    ))),
     tabItems(
       # First tab content
       tabItem(tabName = "Welcome",
@@ -23,14 +33,24 @@ dashboardPagePlus(
                   width = 12,
                   userList(
                     userListItem(
-                      src = "https://raw.githubusercontent.com/Sanrrone/ESCLAVO/master/images/16srrna.png", 
-                      user_name = "16S", 
-                      description = "28.04.2018"
+                      src = dataURI(file="images/16srrna.png", mime="image/png"), 
+                      user_name = "16S, 18S/ITS", 
+                      description = "Updated: August 2019"
                     ),
                     userListItem(
-                      src = "https://www.rstudio.com/wp-content/uploads/2014/04/knitr.png", 
-                      user_name = "knitr", 
-                      description = "28.04.2018"
+                        src = dataURI(file="images/rnaseq.jpg", mime="image/png"), 
+                        user_name = "RNA-seq", 
+                        description = "Updated: August 2019"
+                    ),
+                    userListItem(
+                      src = dataURI(file="images/metagenomic.jpg", mime="image/png"), 
+                      user_name = "Metagenomic", 
+                      description = "Updated: August 2019"
+                    ),
+                    userListItem(
+                      src = dataURI(file="images/gbs.jpg", mime="image/png"), 
+                      user_name = "Genotype by sequencing", 
+                      description = "Updated: August 2019"
                     )
                   )
                 )
@@ -39,12 +59,39 @@ dashboardPagePlus(
                 box(
                   title = "Create new project",
                   status = "success",
-                  width = 6
+                  width = 4,
+                  actionBttn(
+                    inputId = "newprojectbtn",
+                    label = "Start",
+                    style = "material-flat",
+                    color = "success",
+                    icon = icon("rocket")
+                  )
                 ),
                 box(
-                  title = "Open project",
+                  title = "Search existing project",
                   status = "success",
-                  width = 6
+                  width = 4,
+                  actionBttn(
+                    inputId = "openprojectbtn",
+                    label = "Open",
+                    style = "material-flat",
+                    color = "success",
+                    icon = icon("folder")
+                  )
+                  #shinyDirButton("openprojectbtn", "Chose directory", "Upload")
+                ),
+                box(
+                  title = "Change project directory",
+                  status = "success",
+                  width = 4,
+                  actionBttn(
+                    inputId = "changeprojectdirbtn",
+                    label = "Choose",
+                    style = "material-flat",
+                    color = "success",
+                    icon = icon("gears")
+                  )
                 )
               )
               
