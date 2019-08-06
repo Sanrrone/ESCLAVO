479b51ef83f637a8d3950fc74e6835830853c892
sanrrone <sandrolvalenzuelad@gmail.com>
2019-08-06 12:41:20 -0400

    only allow 16s pipeline

--- Modules/functions.R
+++ Modules/functions.R
@@ -1,6 +1,6 @@
 
 #analysisAvail=c("16s18sits"=1,"rnaseq"=2,"metagenomic"=3,"gbs"=4)
-analysisAvail=c("16s18sits"=1,"metagenomic"=3)
+analysisAvail=c("16s18sits"=1)
 
 pipelines<-list()
 for(module in names(analysisAvail)){
